10a6bd59812c33bd3cca30ac81a5d8ccd1f7c3ec
Albert Jozsa-Kiraly <jkalbert96@gmail.com>
2021-04-19 14:32:39 +0100

    Improve code for registration and implement login.

--- app/src/main/java/com/albertjk/chatapp/LoginFragment.kt
+++ app/src/main/java/com/albertjk/chatapp/LoginFragment.kt
@@ -5,17 +5,23 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_login.*
-import kotlinx.android.synthetic.main.fragment_register.*
 
 class LoginFragment : Fragment(), View.OnClickListener {
 
-    private val TAG = LoginFragment::class.qualifiedName
+    private val TAG = this::class.qualifiedName
 
     private lateinit var navController: NavController
+
+    // The shared instance of the FirebaseAuth object.
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +36,9 @@ class LoginFragment : Fragment(), View.OnClickListener {
 
         navController = Navigation.findNavController(view)
 
+        // Initialise Firebase Auth
+        auth = Firebase.auth
+
         loginButton.setOnClickListener(this)
         backToRegistrationTextView.setOnClickListener(this)
     }
@@ -37,14 +46,52 @@ class LoginFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v) {
             loginButton -> {
-
-                val email = emailTextInputLayout_login.editText?.text.toString()
-                val password = passwordTextInputLayout_login.editText?.text.toString()
-
-                Log.d(TAG, "Attempted login")
+                login()
             }
 
             backToRegistrationTextView -> navController.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+    /**
+     * Get the user's input and log them in.
+     */
+    private fun login() {
+        val email = emailTextInputLayout_login.editText?.text.toString().trim()
+        val password = passwordTextInputLayout_login.editText?.text.toString().trim()
+
+
+        Log.d(TAG, "Attempted login")
+        Log.d(TAG, "email is $email")
+        Log.d(TAG, "password is $password")
+
+        /* If the user did not fill out a field, tell them.
+        Otherwise, try to log them in. */
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this.context, "Please enter your login credentials.", Toast.LENGTH_LONG).show()
+        } else {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    // Log in successful.
+                    if (it.isSuccessful) {
+                        Log.d(TAG, "signInWithEmailAndPassword: success")
+                        Log.d(TAG, "Successfully signed in user with uid: ${it.result!!.user.uid}")
+
+
+
+                        // TODO: Redirect user to the Messages fragment.
+
+                        //val user = auth.currentUser
+
+                    }
+                    /* If it fails, display a message to the user.
+                    Input validation errors are displayed here by Firebase Auth. */
+                    else {
+                        Log.w(TAG, "signInWithEmailAndPassword: failure", it.exception)
+                        Log.d(TAG, "Exception message: " + it.exception!!.message)
+                        Toast.makeText(this.context, it.exception!!.message, Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 
--- app/src/main/java/com/albertjk/chatapp/RegisterFragment.kt
+++ app/src/main/java/com/albertjk/chatapp/RegisterFragment.kt
@@ -52,47 +52,52 @@ class RegisterFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v) {
             registerButton -> {
-                val username = usernameTextInputLayout_register.editText?.text.toString().trim()
-                val email = emailTextInputLayout_register.editText?.text.toString().trim()
-                val password = passwordTextInputLayout_register.editText?.text.toString().trim()
-
-                Log.d(TAG, "Attempted registration.")
-
-                Log.d(TAG, "username is $username")
-                Log.d(TAG, "email is $email")
-                Log.d(TAG, "password is $password")
-
-                // TODO: PERFORM INPUT VALIDATION.
-
-
-
-                // Create a new user account with the email and the password.
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener {
-                        // If it fails, display a message to the user.
-                        if (it.isSuccessful) {
-                            Log.d(TAG, "Successfully created user with uid: ${it.result!!.user.uid}")
-                            Toast.makeText(this.context, "Account created.", Toast.LENGTH_SHORT).show()
-
-                            // val user = auth.currentUser
-                        }
-                        else {
-
-
-                            Log.w(TAG, "createUserWithEmail: failure", it.exception)
-                            Log.d(TAG, "Exception message:" + it.exception!!.message)
-                            Toast.makeText(this.context, "Authentication failed.", Toast.LENGTH_SHORT).show()
-
-
-                        }
-                    }
-
-
-
+                register()
             }
 
             alreadyHaveAnAccountTextView -> navController.navigate(R.id.action_registerFragment_to_loginFragment)
         }
     }
 
+    /**
+     * Get the user's input and create a new account for them.
+     */
+    private fun register() {
+        val username = usernameTextInputLayout_register.editText?.text.toString().trim()
+        val email = emailTextInputLayout_register.editText?.text.toString().trim()
+        val password = passwordTextInputLayout_register.editText?.text.toString().trim()
+
+        Log.d(TAG, "Attempted registration.")
+        Log.d(TAG, "username is $username")
+        Log.d(TAG, "email is $email")
+        Log.d(TAG, "password is $password")
+
+        /* If the user did not fill out a field, tell them.
+        Otherwise, create a new user account with the email and the password. */
+        if(username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this.context, "Please enter valid input.", Toast.LENGTH_LONG).show()
+        } else {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    // Registration successful.
+                    if (it.isSuccessful) {
+                        Log.d(TAG, "Successfully created user with uid: ${it.result!!.user.uid}")
+                        Toast.makeText(this.context, "Account created.", Toast.LENGTH_SHORT).show()
+
+                        // TODO:
+                        // The user is automatically logged in, so redirect them the Messages fragment.
+
+                        // val user = auth.currentUser
+                    }
+                    /* If it fails, display a message to the user.
+                    Input validation errors are displayed here by Firebase Auth.
+                    If the e-mail is already in use, a message is displayed here by Firebase Auth. */
+                    else {
+                        Log.w(TAG, "createUserWithEmail: failure", it.exception)
+                        Log.d(TAG, "Exception message: " + it.exception!!.message)
+                        Toast.makeText(this.context, it.exception!!.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
 }
