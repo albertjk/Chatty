c9aeaeb57fa33be259ab7e6f9c06df4421a46f42
Albert Jozsa-Kiraly <jkalbert96@gmail.com>
2021-04-28 18:53:45 +0100

    Make Latest Messages fragment the first screen to be shown when the app is launched

--- app/src/main/java/com/albertjk/chatapp/LatestMessagesFragment.kt
+++ app/src/main/java/com/albertjk/chatapp/LatestMessagesFragment.kt
@@ -5,8 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LatestMessagesFragment : Fragment() {
+
+    private val TAG = this::class.qualifiedName
+
+    private lateinit var navController: NavController
+
+    // The shared instance of the FirebaseAuth object.
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +27,31 @@ class LatestMessagesFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_latest_messages, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        // Initialise Firebase Auth.
+        auth = Firebase.auth
+
+        checkUserIsLoggedIn()
+
+    }
+
+    /**
+     * Checks if the user is logged in to the app using Firebase Auth.
+     */
+    private fun checkUserIsLoggedIn() {
+
+        // If the user is not logged in, redirect them to the Register fragment.
+        if (auth.uid == null) {
+            navController.navigate(R.id.action_latestMessagesFragment_to_registerFragment)
+        }
+    }
+
+
 
 
 }
