6c57942d848999a52bfab40e2d30a046ac66b3e5
Albert Jozsa-Kiraly <jkalbert96@gmail.com>
2023-07-18 21:48:29 +0100

    Get user from database and display its photo and username in the recycler view.

--- app/src/main/java/com/albertjk/chatapp/NewMessageFragment.kt
+++ app/src/main/java/com/albertjk/chatapp/NewMessageFragment.kt
@@ -1,6 +1,7 @@
 package com.albertjk.chatapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +12,12 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.albertjk.chatapp.databinding.FragmentNewMessageBinding
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 class NewMessageFragment : Fragment() {
@@ -20,11 +26,12 @@ class NewMessageFragment : Fragment() {
 
     private lateinit var navController: NavController
 
-    // The shared instance of the FirebaseAuth object.
-    private lateinit var auth: FirebaseAuth
+    private lateinit var dbReference: DatabaseReference
 
     private var _binding: FragmentNewMessageBinding? = null
     private val binding get () = _binding!!
+
+    private lateinit var usersRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,17 +49,41 @@ class NewMessageFragment : Fragment() {
 
         (activity as AppCompatActivity?)!!.supportActionBar?.title = "Select User"
 
-        val usernameList: List<String> = listOf("John", "Jen", "Jack")
+        initUsersRecyclerView()
 
-        initUsersRecyclerView(usernameList)
+        dbReference = Firebase.database.reference
 
-
-
+        getUsersFromFirebase(dbReference)
     }
 
-    private fun initUsersRecyclerView(usernameList: List<String>) {
-        val usersRecyclerView: RecyclerView = binding.newMessageRecyclerView.findViewById(R.id.newMessageRecyclerView)
-        val usersAdapter = activity?.let { UsersAdapter(usernameList) }
+    private fun getUsersFromFirebase(dbReference: DatabaseReference) {
+
+        val listener = object:ValueEventListener {
+            // onDataChange is called every time we retrieve all users from the database.
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userList = mutableListOf<User>()
+
+                snapshot.children.forEach { it.children.forEach {child ->
+                    Log.d(TAG, child.toString())
+                    val user = child.getValue(User::class.java)
+                    if (user != null) {
+                        userList.add(user)
+                    }
+                 } }
+                val usersAdapter = activity?.let { UsersAdapter(userList) }
+                usersRecyclerView.adapter = usersAdapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG, "Getting users from database is cancelled. Error: $error")
+            }
+        }
+        dbReference.addValueEventListener(listener)
+    }
+
+    private fun initUsersRecyclerView() {
+        usersRecyclerView = binding.newMessageRecyclerView.findViewById(R.id.newMessageRecyclerView)
+        val usersAdapter = activity?.let { UsersAdapter(mutableListOf()) }
         usersRecyclerView.layoutManager = LinearLayoutManager(activity)
         usersRecyclerView.adapter = usersAdapter
     }
--- app/src/main/java/com/albertjk/chatapp/UsersAdapter.kt
+++ app/src/main/java/com/albertjk/chatapp/UsersAdapter.kt
@@ -1,25 +1,15 @@
 package com.albertjk.chatapp
 
-import android.content.Context
-import android.media.Image
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.albertjk.chatapp.databinding.FragmentNewMessageBinding
 import com.albertjk.chatapp.databinding.UserRowBinding
-import de.hdodenhof.circleimageview.CircleImageView
+import com.squareup.picasso.Picasso
 
-class UsersAdapter(private val userList: List<String>):
+class UsersAdapter(private val userList: MutableList<User>):
     RecyclerView.Adapter<UsersAdapter.UsersViewHolder>() {
 
     private val TAG = this::class.qualifiedName
-
-//    private val usernames: List<String> = listOf("John")
-
-    private var _binding: FragmentNewMessageBinding? = null
-    private val binding get () = _binding!!
 
     inner class UsersViewHolder(val binding: UserRowBinding): RecyclerView.ViewHolder(binding.root)
 
@@ -34,7 +24,8 @@ class UsersAdapter(private val userList: List<String>):
     override fun onBindViewHolder(holder: UsersAdapter.UsersViewHolder, position: Int) {
         with(holder) {
             with(userList[position]) {
-                binding.usernameTextView.text = userList[position]
+                Picasso.get().load(userList[position].profileImageUrl).into(binding.photoImageViewUser)
+                binding.usernameTextView.text = userList[position].username
             }
         }
     }
