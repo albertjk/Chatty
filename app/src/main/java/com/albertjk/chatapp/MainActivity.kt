23a7fea772b0a1e64c2ac93db1132f39e27916ab
Albert Jozsa-Kiraly <jkalbert96@gmail.com>
2023-07-19 20:52:26 +0100

    Set up back navigation in the action bar.

--- app/src/main/java/com/albertjk/chatapp/MainActivity.kt
+++ app/src/main/java/com/albertjk/chatapp/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.albertjk.chatapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            //  IDs of fragments without the ActionBar up button
+            setOf(R.id.registerFragment, R.id.loginFragment, R.id.latestMessagesFragment))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    /**
+     * Allows Navigation to support ActionBar up navigation.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+    }
+
+    /**
+     * Makes the Navigation UI handle the ActionBar item selection.
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 }
